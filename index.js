02ea862b2eb5caed6977c76b41ecfbf84df97f39
Ace <39498818+KironDevCoder@users.noreply.github.com>
2021-03-28 20:14:25 +0200

    First version

---
+++ index.js
@@ -0,0 +1,24 @@
+const secret = require("./secret.js");
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "$";
+
+client.on("message", function(message) {
+  if (message.author.bot) return; // Sender was a bot
+
+  // Global
+
+  if (message.content.startsWith(prefix)) {
+    // Command
+  } else {
+    // Text
+  }
+});
+
+client.on("ready", function() {
+  console.log("Logged in as " + client.user.tag + "!");
+});
+
+client.login(secret.DISCORD_TOKEN);
